659da94dada6328aa915b9cc0d005b3befbbc463
almostdevelopr <minhalam.dev@gmail.com>
2022-06-11 21:32:58 +0530

    Initial setup

---
+++ learning_log/ll_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/minhaz/Documents/GitHub/learning-log/learning_log/ll_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
