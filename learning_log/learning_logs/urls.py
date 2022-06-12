46a093bd2635c8d2131348903dd479a23e34ebfe
almostdevelopr <minhalam.dev@gmail.com>
2022-06-12 13:39:01 +0530

    Add topic wise entries

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     # Home page
     path("", views.index, name="index"),
     path("topics/", views.topics, name="topics"),
+    # Define page for a single topic.
+    path("topics/<int:topic_id>/", views.topic, name="topic"),
 ]
