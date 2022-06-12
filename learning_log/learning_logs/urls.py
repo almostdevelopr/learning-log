550f6e8ee34b0b5691b0b05bca2c4e5644488409
almostdevelopr <minhalam.dev@gmail.com>
2022-06-12 12:43:30 +0530

    Add topics and home page

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -4,8 +4,9 @@ from django.urls import path
 
 from . import views
 
-app_name = "learning_logs"
+app_name = "learning_logs"  # namespace
 urlpatterns = [
     # Home page
     path("", views.index, name="index"),
+    path("topics/", views.topics, name="topics"),
 ]
